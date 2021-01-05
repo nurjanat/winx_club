4978195557420346e4e56bc2171fd6895c13e075
nurjanat <tungatarovva@gmail.com>
2021-01-05 15:14:55 +0300

    solve homework

--- aaa.py
+++ aaa.py
@@ -5,20 +5,29 @@
 #     x = x + x * 0,1
 #     print("день номер:", i,"расстояние",x)
 
-username = 'maksimka'
-password = '123456'
+# username = 'maksimka'
+# password = '123456'
 
-def sign_in():
-    tries = 0
-    while tries < 3:
-        check_username = input()
-        check_password = input()
-        if  check_username == username and check_password == password:
-            print('вы успешно вошли')
-            break
-        else:
-            print('вы ввели неверные данные')
-            tries += 1
-sign_in()
+# def sign_in():
+#     tries = 0
+#     while tries < 3:
+#         check_username = input()
+#         check_password = input()
+#         if  check_username == username and check_password == password:
+#             print('вы успешно вошли')
+#             break
+#         else:
+#             print('вы ввели неверные данные')
+#             tries += 1
+# sign_in()
 
 
+list1=[10,20,30,40]
+list2 = []
+i = 0
+while i < len(list1):
+    list2.insert(0,list[i])
+    i = i + 1
+    # print(list2)
+print(list1)
+print(list2)
