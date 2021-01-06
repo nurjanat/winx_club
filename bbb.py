55964e57b8b7df21e8886e28dc5276db77bdbb61
nurjanat <tungatarovva@gmail.com>
2021-01-06 12:04:28 +0300

    hw

--- bbb.py
+++ bbb.py
@@ -1,7 +1,19 @@
 list1 = [1,2,3,4,5]
-i  = 0
-result = list1[0]
+i = 0
+result = 0
 while i < len(list1):
     result = result + list1[i]
     i = i + 1
 print(result)
+
+
+
+# list = [1,2,3,4,5,6,7,8,9,9]
+# x = 9
+# i = 0
+# while x < len(list):
+#     result = x + list[0]
+#     i = i + x
+# print(result)
+
+
