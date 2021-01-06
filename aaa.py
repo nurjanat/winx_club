82121fe0553633ffdbfedcc24411c2df14d9e0a2
nurjanat <tungatarovva@gmail.com>
2021-01-06 08:11:05 +0300

    hw

--- aaa.py
+++ aaa.py
@@ -7,7 +7,7 @@
 
 # username = 'maksimka'
 # password = '123456'
-
+#
 # def sign_in():
 #     tries = 0
 #     while tries < 3:
@@ -22,12 +22,17 @@
 # sign_in()
 
 
-list1=[10,20,30,40]
-list2 = []
+# list1 = [10,20,30,40]
+# list2 = []
+# i = 0
+# while i < len(list1):
+#     list2.insert(0,list1[i])
+#     i = i + 1
+# print(list2)
+
+
+
+list1 = [1,2,3,4,5]
 i = 0
+result = sum(list1)
 while i < len(list1):
-    list2.insert(0,list[i])
-    i = i + 1
-    # print(list2)
-print(list1)
-print(list2)
---
+++ bbb.py
@@ -0,0 +1,7 @@
+list1 = [1,2,3,4,5]
+i  = 0
+result = list1[0]
+while i < len(list1):
+    result = result + list1[i]
+    i = i + 1
+print(result)
