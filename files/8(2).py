95c668858d0a01055e0b69253ecd6d3b7b59f130
nurjanat <tungatarovva@gmail.com>
2021-01-07 15:06:31 +0300

    hw

---
+++ files/8(2).py
@@ -0,0 +1,10 @@
+list1 = [1,1,2,3,3,4,5,4,6,6,7,7,8,8,9,9]
+# list2 = []
+# i = 0
+# while i < len(list1):
+#     if list1[i] not in list2:
+#         list2.append(list1[i])
+#     i += 1
+# print(list2)
+
+print(set(list1))
