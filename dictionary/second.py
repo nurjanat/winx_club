ffe19f48e2a7e3ed8c081c224cb755e4cf2c0c3b
nurjanat <tungatarovva@gmail.com>
2021-01-11 12:34:35 +0300

    loop for

--- dictionary/second.py
+++ dictionary/second.py
@@ -1,37 +1,37 @@
-# username = "nurjanat"
-# password = "123456"
-# def auth(check_user,check_pass):
-#     if check_user == username and check_pass == password:
-#         print("ok")
-#     else:
-#         print("not ok")
+username = "nurjanat"
+password = "123456"
+def auth(check_user,check_pass):
+    if check_user == username and check_pass == password:
+        print("ok")
+    else:
+        print("not ok")
 
 
-# auth(username,password)
+auth(username,password)
+#
+# /
+dict1 = {"phones":["iphone 6","iphone 7","iphone 8","iphone 10"],
+         "clothes":["jeans","cap","skirt","shirt"],
+         "food":["pizza","doner","hamburger"]
+         }
 
-
-# dict1 = {"phones":["iphone 6","iphone 7","iphone 8","iphone 10"],
-#          "clothes":["jeans","cap","skirt","shirt"],
-#          "food":["pizza","doner","hamburger"]
-#          }
-
-# dict1['phones'].append(4)
-# print(dict1)
+dict1['phones'].append(4)
+print(dict1)
 #
 
 
 
 
-# list1 = ['iphone6','iphone7','coat','jeans','pizza','coca cola']
+list1 = ['iphone6','iphone7','coat','jeans','pizza','coca cola']
+#
 
 
-
-# for product in list1:
-#     if product == 'iphone6' or product == 'iphone7' :
-#         file1 = open('phone.txt','a')
-#         file1.write(product)
-#     elif product == 'coat' or product == 'jeans':
-#         file2 = open('shop.txt','a')
+for product in list1:
+    if product == 'iphone6' or product == 'iphone7' :
+        file1 = open('phone.txt','a')
+        file1.write(product)
+    elif product == 'coat' or product == 'jeans':
+        file2 = open('shop.txt','a')
 #         file2.write(product)
 #     else:
 #         file3 = open('food.txt','a')
